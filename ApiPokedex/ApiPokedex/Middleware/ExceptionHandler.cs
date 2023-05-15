9d164984b5c764e0ff165f8369ac815dbe9f3d7d
jvpmiranda <joao.miranda95@hotmail.com>
2023-05-15 16:21:16 -0300

    Inicio do projeto
    Commit inicial do projeto

---
+++ ApiPokedex/ApiPokedex/Middleware/ExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiPokedex.Middleware
+{
+    public class ExceptionHandlerMiddleware : IMiddleware
+    {
+        public Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                return next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+
+                return Task.FromException(ex);
+            }
+        }
+    }
+}
