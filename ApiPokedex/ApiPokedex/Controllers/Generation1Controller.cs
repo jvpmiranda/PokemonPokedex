ff74e3d878a1bdedb7343276220af5e0bf4691cb
jvpmiranda <joao.miranda95@hotmail.com>
2023-05-16 06:49:56 -0300

    Operações básicas do controller
    Operações ainda não implementadas, apenas definidas.

---
+++ ApiPokedex/ApiPokedex/Controllers/Generation1Controller.cs
@@ -0,0 +1,46 @@
+using ApiPokedex.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace ApiPokedex.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    //[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    public class Generation1Controller : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAll")]
+        public IEnumerable<Pokemon> GetAll()
+        {
+            throw new Exception("Erro no metodo GET");
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public Pokemon Get([FromQuery]int pokemonId)
+        {
+            throw new Exception($"Erro no metodo GET com o parametro {id}");
+        }
+
+        [HttpPost]
+        public ActionResult Post(Pokemon pokemon)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put(Pokemon pokemon)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int pokemonId)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ApiPokedex/ApiPokedex/Models/Pokemon.cs
@@ -0,0 +1,9 @@
+namespace ApiPokedex.Models
+{
+    public class Pokemon
+    {
+        public int PokemonId { get; set; }
+
+        public string PokemonName { get; set; }
+    }
+}
